217ae55022220d1e44a710a7b51001edf7cfc36b
Xiang Zhang <xzhang@thoughtworks.com>
2016-06-10 15:55:23 +0800

    add playview ui.

--- PlayView.js
+++ PlayView.js
@@ -9,6 +9,7 @@ var {
   Text,
   View,
   TouchableOpacity,
+  ProgressViewIOS,
 } = ReactNative;
 
 // require the module
@@ -16,35 +17,16 @@ var RNFS = require('react-native-fs');
 
 var uploadUrl = 'http://buz.co/rnfs/upload-tester.php';  // For testing purposes, go to http://requestb.in/ and create your own link
 
-  // <ScrollView contentContainerStyle={styles.contentContainer}>
-  //   <View style={styles.mainSection}>
-  //     <Image
-  //       source={getImageSource(this.props.movie, 'det')}
-  //       style={styles.detailsImage}
-  //     />
-  //     <View style={styles.rightPane}>
-  //       <Text style={styles.movieTitle}>{this.props.movie.title}</Text>
-  //       <Text>{this.props.movie.year}</Text>
-  //       <View style={styles.mpaaWrapper}>
-  //         <Text style={styles.mpaaText}>
-  //           {this.props.movie.mpaa_rating}
-  //         </Text>
-  //       </View>
-  //       <Ratings ratings={this.props.movie.ratings} />
-  //     </View>
-  //   </View>
-  //   <View style={styles.separator} />
-  //   <Text>
-  //     {this.props.movie.synopsis}
-  //   </Text>
-  //   <View style={styles.separator} />
-  //   <Cast actors={this.props.movie.abridged_cast} />
-  // </ScrollView>
-
 var PlayView = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
+  getInitialState: function() {
+    return {
+      progress: 1,
+    };
+  },
+
+  renderSubview: function() {
+    if(this.state.progress >= 0 && this.state.progress < 1) {
+      return (<View style={styles.uploadView}>
         <TouchableOpacity
           onPress={this.play}
           activeOpacity={0.6}>
@@ -53,13 +35,57 @@ var PlayView = React.createClass({
             source={require('./images/start-96.png')}
           />
         </TouchableOpacity>
+        <View style={[styles.progressContainer, styles.center]}>
+          <Text>{'文件上传中'}</Text>
+          <ProgressViewIOS
+            style={styles.progressView}
+            progressTintColor="orange"
+            progress={0.8}>
+          </ProgressViewIOS>
+        </View>
+      </View>);
+    }
+    if(this.state.progress == 1) {
+      return (<View style={styles.presentView}>
+        <View style={styles.currentPage}>
+          <Image source={{uri: 'http://img4.cache.netease.com/news/2016/6/10/201606100857298ab4c.jpg'}}
+            style={{flex: 1,}}/>
+        </View>
+        <View style={styles.nextPage}>
+          <Image source={{uri: 'http://img4.cache.netease.com/news/2016/6/10/201606100857298ab4c.jpg'}}
+            style={{flex: 1,}}/>
+        </View>
+        <View style={styles.control}>
+          <TouchableOpacity
+            style={styles.preButton}
+            activeOpacity={0.6}>
+            <Image
+              style={styles.button}
+              source={require('./images/pre-50.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.nextButton}
+            activeOpacity={0.6}>
+            <Image
+              style={styles.button}
+              source={require('./images/next-50.png')}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>);
+    }
+  },
+
+  render: function() {
+    return (
+      <View style={styles.container}>
+        {this.renderSubview()}
       </View>
     );
   },
 
   play: function() {
-    console.log('play......');
-    // create an array of objects of the files you want to upload
     var files = [
       {
         name: 'test.pptx',
@@ -90,94 +116,58 @@ var PlayView = React.createClass({
     // upload files
     RNFS.uploadFiles(options)
       .then(response => {
-        console.log('FILES UPLOADED!');
+        this.setState({progress: 1});
       })
       .catch(err => {
         console.log(err);
       });
-    // var uploadUrl1 = 'http://buz.co/rnfs/upload-tester.php';
-    // var progress1 = data => {
-    //   console.log('uploading...');
-    //   // this.setState({ output: text });
-    // };
-    //
-    // var begin1 = res => {
-    //   // jobId1 = res.jobId;
-    //   console.log('begin upload...');
-    // };
-    //
-    // var options = {
-    //   toUrl: uploadUrl1,
-    //   files: [{ name: 'myfile', filename: this.props.doc.name, filepath: this.props.doc.path, filetype: 'image/jpeg' }],
-    //   beginCallback: begin1,
-    //   progressCallback: progress1
-    // };
-    //
-    // RNFS.uploadFiles(options).then(res => {
-    //   console.log('uploaded...');
-    // }).catch(err => this.showError(err));
   }
 });
 
 var styles = StyleSheet.create({
   container: {
-    marginTop: 64,
+    flex: 1,
+  },
+  uploadView: {
+    flex: 1,
+    // backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent:'center',
+  },
+  progressView: {
+    width: 300,
+    marginTop: 5,
+  },
+  center: {
+    alignItems: 'center',
   },
   startBtn: {
     width: 96,
     height: 96,
   },
-  rightPane: {
-    justifyContent: 'space-between',
+  presentView: {
+    marginTop: 64,
+    marginBottom: 50,
     flex: 1,
   },
-  movieTitle: {
+  currentPage: {
     flex: 1,
-    fontSize: 16,
-    fontWeight: '500',
+    alignItems: 'stretch',
+    padding: 10,
   },
-  rating: {
-    marginTop: 10,
+  nextPage: {
+    flex: 1,
+    alignItems: 'stretch',
+    padding: 20,
   },
-  ratingTitle: {
-    fontSize: 14,
-  },
-  ratingValue: {
-    fontSize: 28,
-    fontWeight: '500',
-  },
-  mpaaWrapper: {
-    alignSelf: 'flex-start',
-    borderColor: 'black',
-    borderWidth: 1,
-    paddingHorizontal: 3,
-    marginVertical: 5,
-  },
-  mpaaText: {
-    fontFamily: 'Palatino',
-    fontSize: 13,
-    fontWeight: '500',
-  },
-  mainSection: {
+  control: {
+    backgroundColor: 'lightgrey',
+    height: 50,
     flexDirection: 'row',
+    justifyContent:'center',
   },
-  detailsImage: {
-    width: 134,
-    height: 200,
-    backgroundColor: '#eaeaea',
-    marginRight: 10,
-  },
-  separator: {
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    height: StyleSheet.hairlineWidth,
-    marginVertical: 10,
-  },
-  castTitle: {
-    fontWeight: '500',
-    marginBottom: 3,
-  },
-  castActor: {
-    marginLeft: 2,
+  nextButton: {
+    marginLeft: 30,
   },
 });
 
